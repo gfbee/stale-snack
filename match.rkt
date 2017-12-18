4063a4f0b14052b0a9cd9073a5cb91edee9e701c
gfbee <gfb@cs.toronto.edu>
2017-12-18 13:15:10 -0500

    separate names from implementations for easier comparison
    and start getting serious about the static and dynamic error messages

--- match.rkt
+++ match.rkt
@@ -9,14 +9,27 @@
  Function versions have names of the form ‘λ-match<>’, emphasizing the public api aspect
   versus implementation details. In particular, ‘match-lambda’ is provided as ‘λ-match’. |#
 
-#| ToDo
- • ‘#:when’ et al
- • examine syntax error messages
- • investigate what's going on when hovering over uses of ‘map-matcher’ |#
-
 (provide λ-match
          λ-match/false    match/false
          λ-match/identity match/identity)
+
+#| ToDo
+ • ‘#:when’ et al
+ • investigate what's going on when hovering over uses of ‘map-matcher’ |#
+
+#| ToDo: Syntax and Runtime error messages |#
+#;(match 0 [])
+#;(match-lambda [])
+; ?: expected more terms starting with any term
+;  parsing context: while parsing a clause with a pattern and a result in: ()
+#;(match 0)
+#;((match-lambda) 0)
+#;(define-syntax (m  stx)
+    (syntax-case stx () [(_ e clauses ...) #`(match         e       clauses ...)]))
+#;(define-syntax (m′ stx)
+    (syntax-case stx () [(_ e clauses ...) #`(match/derived e #,stx clauses ...)]))
+#;(m 0)
+#;(m′0)
 
 (module+ test
 
@@ -36,34 +49,47 @@
   (check-equal? (map-matcher (λ-match/identity match/identity ['a 1])        '(a b))   '(1 b))
   (check-equal? (map-matcher (λ-match/identity match/identity ['a 1] ['b 2]) '(a b c)) '(1 2 c)))
 
-(require (for-syntax syntax/parse racket/base
-                     (only-in "syntax.rkt" syntax-tooltip)))
+(require (for-syntax syntax/parse racket/base (only-in "syntax.rkt" syntax-tooltip)))
          
-(require (only-in racket/match match-lambda match))
+(require (only-in racket/match match/derived match))
 
-(define-syntax λ-match
-  (syntax-parser [(_ clause ...) (syntax-tooltip this-syntax
-                                                 #'(match-lambda clause ...)
-                                                 "• just match-lambda, but renamed")]))
-(define-syntax λ-match/false
-  (syntax-parser [(_ clause ...) (syntax-tooltip this-syntax
-                                  #'(λ (v) (match/false v clause ...))
-                                  "• match-lambda, but that returs #false if no match")]))
-(define-syntax match/false
-  (syntax-parser [(_ e:expr [pattern result:expr ...] ...)
-                  (syntax-tooltip this-syntax
-                                  #'(match e [pattern #true result ...] ... [_ #false])
-                                  "• match, but that produces #false if no match")]))
-(define-syntax λ-match/identity
-  (syntax-parser [(_ clause ...)
-                  (syntax-tooltip this-syntax
-                                  #'(λ (v) (match/identity v clause ...))
-                                  "• match-lambda, but that returns the argument if no match")]))
-(define-syntax   match/identity
-  (syntax-parser [(_ e:expr clause ...)
-                  (syntax-tooltip this-syntax
-                                  #'(match e clause ... [v v])
-                                  "• match, but that produces the value if no match")]))
+(define-for-syntax (with-renamed-syntax-errors name stx)
+  (with-handlers
+      ([exn:fail:syntax? (λ (e) (raise
+                                 (make-exn:fail:syntax
+                                  (string-append name
+                                                 (substring (exn-message e) (string-length "match")))
+                                  (exn-continuation-marks e)
+                                  (exn:fail:syntax-exprs e))))])
+    (local-expand stx (syntax-local-context) '())))
+
+(define-syntaxes (λ-match λ-match/false match/false λ-match/identity match/identity)
+  (values
+   (syntax-parser [(_ clause ...)
+                   (syntax-tooltip this-syntax #'(match-lambda clause ...)
+                                   "• just match-lambda, with a different name")])
+   (syntax-parser [(_ clause ...)
+                   (syntax-tooltip this-syntax #'(λ (v) (match/false v clause ...))
+                                   "• match-lambda, but that returns #false if no match")])
+   (syntax-parser [(_ e:expr [pattern result:expr ...] ...)
+                   (syntax-tooltip this-syntax
+                                   (with-renamed-syntax-errors "match/false"
+                                     #`(match/derived e #,this-syntax
+                                                      [pattern #true result ...]
+                                                      ...
+                                                      [_ #false]))
+                                   "• match, but that produces #false if no match")])
+   (syntax-parser [(_ clause ...)
+                   (syntax-tooltip this-syntax
+                                   #'(λ (v) (match/identity v clause ...))
+                                   "• match-lambda, but that returns the argument if no match")])
+   (syntax-parser [(_ e:expr clause ...)
+                   (syntax-tooltip this-syntax
+                                   #`(match/derived e #,this-syntax
+                                                    clause
+                                                    ...
+                                                    [v v])
+                                   "• match, but that produces the value if no match")])))
 
 
 (provide define/match₁ ‹›)
