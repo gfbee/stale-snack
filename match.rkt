7ce19a0254405b1a23d1e1eb25fba3d2f9c9a73f
gfbee <gfb@cs.toronto.edu>
2017-12-19 13:39:51 -0500

    Place for: deprecated, not-yet-finalized, and meta work.

--- match.rkt
+++ match.rkt
@@ -31,7 +31,7 @@
 #;(m 0)
 #;(m′0)
 
-(module+ test
+(module+ test (require rackunit)
 
   (define-syntax map-matcher
     (syntax-parser [(_ (λ-matcher:id matcher:id clause ...) a-list:expr)
@@ -52,7 +52,7 @@
 (require (for-syntax syntax/parse racket/base
                      (only-in "syntax.rkt" syntax-tooltip with-renamed-syntax-errors)))
          
-(require (only-in racket/match match/derived match))
+(require (only-in racket/match match/derived match-lambda))
 
 (define-syntaxes (λ-match λ-match/false match/false λ-match/identity match/identity)
   (values
@@ -83,58 +83,3 @@
                                    "• match, but that produces the value if no match")])))
 
 
-(provide define/match₁ ‹›)
-
-(module+ test
-  (require racket/match)
-  (check-equal? (match '(1 2 3) [(‹› ‹a› ...) (‹› ‹a› ...)]) '(1 2 3)))
-
-(require (only-in racket/match define/match define-match-expander))
-(module+ test (require rackunit))
-
-; Feature Extension: safer ‘match’, with declared variables and literals, also for result quasiquote.
-
-(define-syntax define/match₁
-  (syntax-parser
-    [(_ (‹function› ‹formal›:id)
-        [‹pattern› ‹result›:expr ...+]
-        ...+)
-     (syntax/loc this-syntax
-       (define/match (‹function› ‹formal›)
-         [(‹pattern›) ‹result› ...]
-         ...))]))
-
-(begin-for-syntax
-  (require (only-in syntax/stx stx-pair? stx-car stx-cdr)
-           racket/match)
-  (define (handle-atom stx)
-    (if (and (syntax? stx) (symbol? (syntax-e stx))
-             (regexp-match-exact? #rx"‹.+›" (symbol->string (syntax-e stx))))
-        (quasisyntax/loc stx ,#,(case (syntax->datum stx)
-                                  [(‹_›) (datum->syntax stx '_)]
-                                  [else stx]))
-        stx))
-  (define pattern-transformer
-    (syntax-parser
-      [(_ . ‹list-elements-pattern›)
-       (quasisyntax/loc this-syntax
-         `#,(let loop ([stx #'‹list-elements-pattern›])
-              (if (stx-pair? stx)
-                  (quasisyntax/loc (if (syntax? stx) stx #'‹list-elements-pattern›)
-                    (#,(loop (stx-car stx)) . #,(loop (stx-cdr stx))))
-                  (handle-atom stx))))]))
-  (define expression-transformer
-    (syntax-parser
-      [(_ . ‹list-elements-pattern›)
-       (quasisyntax/loc this-syntax
-         `#,(let loop ([stx #'‹list-elements-pattern›])
-              (define-syntax-rule (quasi new-stx)
-                (quasisyntax/loc (if (syntax? stx) stx #'‹list-elements-pattern›) new-stx))
-              (if (stx-pair? stx)
-                  (match (stx-cdr stx)
-                    [`(,(app syntax->datum '...) . ,_)
-                     (quasi (,@`#,(loop (stx-car stx)) . #,(loop (stx-cdr (stx-cdr stx)))))]
-                    [_  (quasi (#,(loop (stx-car stx)) . #,(loop (stx-cdr stx))))])
-                  (handle-atom stx))))])))
-;
-(define-match-expander ‹› pattern-transformer expression-transformer)
